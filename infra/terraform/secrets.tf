2b015a95f51abb3ea8023694f5236106eca8da80
Nadira <Nadiraaltybaeva93@gmail.com>
2025-12-11 00:38:48 -0600

    Add production webapp, WAF, secrets, monitoring

--- infra/terraform/secrets.tf
+++ infra/terraform/secrets.tf
@@ -1,9 +1,9 @@
 resource "aws_secretsmanager_secret" "confluent_api" {
-  name = "${var.project_name}-confluent-api"
+  name = "${var.project_name}-confluent-api-v2"
 }
 
 resource "aws_secretsmanager_secret_version" "confluent_api_v1" {
-  secret_id = aws_secretsmanager_secret.confluent_api.id
+  secret_id     = aws_secretsmanager_secret.confluent_api.id
   secret_string = jsonencode({
     api_key    = var.confluent_api_key
     api_secret = var.confluent_api_secret
@@ -11,11 +11,11 @@ resource "aws_secretsmanager_secret_version" "confluent_api_v1" {
 }
 
 resource "aws_secretsmanager_secret" "rds_credentials" {
-  name = "${var.project_name}-rds-credentials"
+  name = "${var.project_name}-rds-credentials-v2"
 }
 
 resource "aws_secretsmanager_secret_version" "rds_credentials_v1" {
-  secret_id = aws_secretsmanager_secret.rds_credentials.id
+  secret_id     = aws_secretsmanager_secret.rds_credentials.id
   secret_string = jsonencode({
     username = "postgres"
     password = var.rds_password
